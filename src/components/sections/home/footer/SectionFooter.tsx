9fc6b7c9d7058f426191b673889685d6ea803300
Sami bentaiba <140959042+samibentaiba@users.noreply.github.com>
2025-03-28 22:42:21 +0100

    refactor(SectionFooter): restructure component with improved styling and layout, migrate styles to CSS file

--- src/components/sections/home/footer/SectionFooter.tsx
+++ src/components/sections/home/footer/SectionFooter.tsx
@@ -1,68 +1,119 @@
-import FooterNav from "./ui/FooterNav";
-import FooterSocial from "./ui/FooterSocial";
-import FooterLocation from "./ui/FooterLocation";
-import FooterPoweredBy from "./ui/FooterPoweredBy";
 import { ReactElement } from "react";
 
-export default function SectionFooter(): ReactElement {
+const classes: Record<
+  | "footer"
+  | "container"
+  | "leftSection"
+  | "title"
+  | "menuItem"
+  | "copyright"
+  | "rightSection"
+  | "sectionTitle"
+  | "sectionText"
+  | "iconsContainer"
+  | "icon"
+  | "poweredByContainer"
+  | "poweredByItem1"
+  | "poweredByItem2"
+  | "poweredByItem3"
+  | "poweredByItem4",
+  string
+> = {
+  footer:
+    "mx-auto w-full px-4 sm:px-6 md:px-8 lg:px-10 max-w-[1880px] flex flex-col items-center py-4",
+  container:
+    "self-stretch bg-zinc-200 rounded-[48px] inline-flex justify-between items-center",
+  leftSection: "px-20 inline-flex flex-col justify-start items-start gap-16",
+  title: "justify-start text-stone-900 text-3xl font-bold font-Syne uppercase",
+  menuItem:
+    "self-stretch justify-start text-stone-900 text-xl font-normal font-Syne leading-loose",
+  copyright:
+    "justify-start text-stone-900 text-opacity-60 text-xl font-normal font-Syne leading-loose",
+  rightSection:
+    "w-[547px] h-full p-20 bg-blue-700 gap-20 rounded-[48px] inline-flex flex-col justify-start items-start g",
+  sectionTitle:
+    "self-stretch justify-start text-white text-3xl font-bold font-Syne uppercase",
+  sectionText:
+    "self-stretch justify-start text-white text-opacity-80 text-xl font-normal font-Syne leading-loose",
+  iconsContainer: "inline-flex justify-start items-center gap-5",
+  icon: "w-7 h-7 bg-white bg-opacity-80",
+  poweredByContainer: "w-9 h-7 relative overflow-hidden",
+  poweredByItem1:
+    "w-1.5 h-3.5 left-0 top-[16.68px] absolute bg-white bg-opacity-80",
+  poweredByItem2: "w-9 h-5 left-0 top-[8.32px] absolute bg-white bg-opacity-80",
+  poweredByItem3:
+    "w-5 h-3.5 left-[16.85px] top-[16.68px] absolute bg-white bg-opacity-80",
+  poweredByItem4:
+    "w-1.5 h-1.5 left-[8.43px] top-0 absolute bg-white bg-opacity-80",
+};
+export default function SectionFooter() {
+  const menuItems = ["Home", "About", "Agenda", "Speakers"];
+  const socialIcons = Array(3).fill(null);
+  const poweredByItems = Array(4).fill(null);
+
   return (
-    <footer  className="mx-auto w-full px-4 sm:px-6 md:px-8 lg:px-10 max-w-[1880px] flex flex-col items-center py-4">
-      
-      <div className="self-stretch bg-zinc-200 rounded-[48px] inline-flex justify-between items-center">
-        <div className="p-20 inline-flex flex-col justify-start items-start gap-20">
-          <div className="justify-start text-stone-900 text-3xl font-bold font-Syne uppercase">
-            ITC Talks
-          </div>
+    <footer className={classes.footer}>
+      <div className={classes.container}>
+        <div className={classes.leftSection}>
+          <div className={classes.title}>ITC Talks</div>
           <div className="flex flex-col justify-start items-start gap-4">
-            <div className="self-stretch justify-start text-stone-900 text-xl font-normal font-Syne leading-loose">
-              Home
-            </div>
-            <div className="self-stretch justify-start text-stone-900 text-xl font-normal font-Syne leading-loose">
-              About
-            </div>
-            <div className="self-stretch justify-start text-stone-900 text-xl font-normal font-Syne leading-loose">
-              Agenda
-            </div>
-            <div className="self-stretch justify-start text-stone-900 text-xl font-normal font-Syne leading-loose">
-              Speakers
-            </div>
+            {menuItems.map((item, index) => (
+              <div key={index} className={classes.menuItem}>
+                {item}
+              </div>
+            ))}
           </div>
-          <div className="justify-start text-stone-900 text-opacity-60 text-xl font-normal font-Syne leading-loose">
+          <div className={classes.copyright}>
             © 2025 Copyright ITC Talks. All rights reserved.
           </div>
         </div>
-        <div className="w-[547px] p-20 bg-blue-700 rounded-[48px] inline-flex flex-col justify-start items-start gap-20">
-          <div className="self-stretch flex flex-col justify-start items-start gap-4">
-            <div className="self-stretch justify-start text-white text-3xl font-bold font-Syne uppercase">
-              Event Location
+        <div className={classes.rightSection}>
+          <InfoSection
+            title="Event Location"
+            content="Auditorium, Saad Dahlab University"
+          />
+          <InfoSection title="Follow US">
+            <div className={classes.iconsContainer}>
+              {socialIcons.map((_, index) => (
+                <div key={index} className={classes.icon} />
+              ))}
             </div>
-            <div className="self-stretch justify-start text-white text-opacity-80 text-xl font-normal font-Syne leading-loose">
-              Auditorium, Saad Dahlab University
+          </InfoSection>
+          <InfoSection title="Powered By">
+            <div className={classes.poweredByContainer}>
+              {poweredByItems.map((_, index) => (
+                <div
+                  key={index}
+                  className={
+                    classes[`poweredByItem${index + 1}` as keyof typeof classes]
+                  }
+                />
+              ))}
             </div>
-          </div>
-          <div className="self-stretch flex flex-col justify-start items-start gap-4">
-            <div className="self-stretch justify-start text-white text-3xl font-bold font-Syne uppercase">
-              Follow US
-            </div>
-            <div className="inline-flex justify-start items-center gap-5">
-              <div className="w-7 h-7 bg-white bg-opacity-80" />
-              <div className="w-7 h-7 bg-white bg-opacity-80" />
-              <div className="w-7 h-7 bg-white bg-opacity-80" />
-            </div>
-          </div>
-          <div className="self-stretch flex flex-col justify-start items-start gap-4">
-            <div className="self-stretch justify-start text-white text-3xl font-bold font-Syne uppercase">
-              Powered By
-            </div>
-            <div className="w-9 h-7 relative overflow-hidden">
-              <div className="w-1.5 h-3.5 left-0 top-[16.68px] absolute bg-white bg-opacity-80" />
-              <div className="w-9 h-5 left-0 top-[8.32px] absolute bg-white bg-opacity-80" />
-              <div className="w-5 h-3.5 left-[16.85px] top-[16.68px] absolute bg-white bg-opacity-80" />
-              <div className="w-1.5 h-1.5 left-[8.43px] top-0 absolute bg-white bg-opacity-80" />
-            </div>
-          </div>
+          </InfoSection>
         </div>
       </div>
     </footer>
+  );
+}
+
+function InfoSection({
+  title,
+  content,
+  children,
+}: {
+  title: string;
+  content?: string;
+  children?: ReactElement;
+}) {
+  return (
+    <div className="self-stretch flex flex-col justify-start items-start gap-4">
+      <div className={classes.sectionTitle}>{title}</div>
+      {content ? (
+        <div className={classes.sectionText}>{content}</div>
+      ) : (
+        children
+      )}
+    </div>
   );
 }
